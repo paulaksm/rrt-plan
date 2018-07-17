168d00c972e5082556ca51b1b3a5f67497b1bbca
paulaksm <paulaksm@ime.usp.br>
2018-07-17 12:00:22 -0300

    Add more tests

--- planner/tests/test1.sh
+++ planner/tests/test1.sh
@@ -47,3 +47,103 @@ echo
 echo '>> Solving pddl/robot/boxes/problem10.pddl ...'
 echo
 python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem10.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem11.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem11.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem12.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem12.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem13.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem13.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem14.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem14.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem15.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem15.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem16.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem16.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem17.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem17.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem18.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem18.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem19.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem19.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem20.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem20.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem21.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem21.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem22.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem22.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem23.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem23.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem24.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem24.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem25.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem25.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem26.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem26.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem27.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem27.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem28.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem28.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem29.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem29.pddl 
+
+echo
+echo '>> Solving pddl/robot/boxes/problem30.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem30.pddl 
--- planner/tests/test1b.sh
+++ planner/tests/test1b.sh
@@ -47,3 +47,103 @@ echo
 echo '>> Solving pddl/robot/rooms/problem10.pddl ...'
 echo
 python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem10.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem11.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem11.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem12.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem12.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem13.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem13.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem14.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem14.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem15.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem15.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem16.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem16.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem17.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem17.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem18.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem18.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem19.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem19.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem20.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem20.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem21.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem21.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem22.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem22.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem23.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem23.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem24.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem24.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem25.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem25.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem26.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem26.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem27.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem27.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem28.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem28.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem29.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem29.pddl 
+
+echo
+echo '>> Solving pddl/robot/rooms/problem30.pddl ...'
+echo
+python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/rooms/problem30.pddl 
